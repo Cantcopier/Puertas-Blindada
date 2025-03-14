15c654f479fd660169861ef5332ba81ccf39c07e
Cantcopier <110771517+Cantcopier@users.noreply.github.com>
2025-03-14 12:02:22 +0000

    add images

--- app/blog-sidebar/blogData.tsx
+++ app/blog-sidebar/blogData.tsx
@@ -1,26 +1,9 @@
 import { Image } from "@/types/image";
 
-const blogData: Image[] = [
-  { id: 1, href: "/images/blog/1.jpeg" },
-  { id: 2, href: "/images/blog/2.jpeg" },
-  { id: 3, href: "/images/blog/3.jpeg" },
-  { id: 4, href: "/images/blog/4.jpeg" },
-  { id: 5, href: "/images/blog/5.jpeg" },
-  { id: 6, href: "/images/blog/6.jpeg" },
-  { id: 7, href: "/images/blog/7.jpeg" },
-  { id: 8, href: "/images/blog/8.jpeg" },
-  { id: 9, href: "/images/blog/9.jpeg" },
-  { id: 10, href: "/images/blog/10.jpeg" },
-  { id: 11, href: "/images/blog/11.jpeg" },
-  { id: 12, href: "/images/blog/12.jpeg" },
-  { id: 13, href: "/images/blog/13.jpeg" },
-  { id: 14, href: "/images/blog/14.jpeg" },
-  { id: 15, href: "/images/blog/15.jpeg" },
-  { id: 16, href: "/images/blog/16.jpeg" },
-  { id: 17, href: "/images/blog/17.jpeg" },
-  { id: 18, href: "/images/blog/18.jpeg" },
-  { id: 19, href: "/images/blog/19.jpeg" },
-  { id: 20, href: "/images/blog/20.jpeg" },
-];
+const blogData: Image[] = Array.from({ length: 57 }, (_, index) => ({
+  id: index + 1,
+  href: `/images/blog/${index + 1}.jpeg`,
+}));
+
 
 export default blogData;
